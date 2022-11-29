2463c7aa15601f40a76d8a18dcfa7800e5b2353a
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-11-29 20:50:33 -0300

    feat: Profile page

---
+++ src/tests/Profile.spec.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+const dataTest = 'data-testid';
+
+const localStorageMock = (() => {
+  let store = { user: '{"email": "rodrigo@gmail.com"}' };
+
+  return {
+    getItem: (key) => store[key] || null,
+    setItem: (key, value) => {
+      store[key] = value.toString();
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
+  };
+})();
+
+describe('Testing Profile page', () => {
+  beforeEach(() => {
+    Object.defineProperty(window, 'localStorage', {
+      value: localStorageMock,
+    });
+  });
+  it('Testing if renders properly', () => {
+    renderWithRouter(<App />, '/profile');
+    const btns = screen.getAllByRole('button');
+    expect(btns).toHaveLength(5);
+    expect(btns[0]).toHaveTextContent('Done Recipes');
+    expect(btns[0]).toHaveAttribute(dataTest, 'profile-done-btn');
+    expect(btns[1]).toHaveTextContent('Favorite Recipes');
+    expect(btns[1]).toHaveAttribute(dataTest, 'profile-favorite-btn');
+    expect(btns[2]).toHaveTextContent('Logout');
+    expect(btns[2]).toHaveAttribute(dataTest, 'profile-logout-btn');
+    const email = screen.getByTestId('profile-email');
+    expect(email).toBeInTheDocument();
+    expect(email).toHaveTextContent('rodrigo@gmail.com');
+  });
+  it('Testing if Done Recipes button redirects to "/done-recipes', () => {
+    const { history } = renderWithRouter(<App />, '/profile');
+    const doneRecipeBtn = screen.getByRole('button', { name: 'Done Recipes' });
+    userEvent.click(doneRecipeBtn);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/done-recipes');
+  });
+  it('Testing if Favorite Recipes button redirects to "/favorite-recipes', () => {
+    const { history } = renderWithRouter(<App />, '/profile');
+    const favRecipeBtn = screen.getByRole('button', { name: 'Favorite Recipes' });
+    userEvent.click(favRecipeBtn);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorite-recipes');
+  });
+  it('Testing if Logout button redirects to "/', () => {
+    const { history } = renderWithRouter(<App />, '/profile');
+    const logoutBtn = screen.getByRole('button', { name: 'Logout' });
+    const user = JSON.parse(localStorage.getItem('user'));
+    expect(user).toEqual({ email: 'rodrigo@gmail.com' });
+    userEvent.click(logoutBtn);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+    const newUser = JSON.parse(localStorage.getItem('user'));
+    expect(newUser).toBeNull();
+  });
+});
