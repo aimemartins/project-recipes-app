c098fc0dab58fcb10bd1dc14c2632d181f597123
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-11-29 16:22:14 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+// import rockGlass from './images/rockGlass.svg';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Meals from './pages/Meals';
+import Drinks from './pages/Drinks';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/meals" component={ Meals } />
+      <Route path="/drinks" component={ Drinks } />
+      {/* <Route
+          path="/meals/:id"
+          render={ (props) => (
+            < { ...props } />
+          ) }
+        />
+        <Route
+          path="/drinks/:id"
+          render={ (props) => (
+            < { ...props } />
+          ) }
+        />
+        <Route
+          path="/meals/:id-da-receita/in-progress"
+          render={ (props) => (
+            < { ...props } />
+          ) }
+        />
+         <Route
+          path="/drinks/:id-da-receita/in-progress"
+          render={ (props) => (
+            < { ...props } />
+          ) }
+        /> */}
+      <Route path="/profile" component={ Profile } />
+      <Route path="/done-recipes" component={ DoneRecipes } />
+    </Switch>
   );
 }
 
