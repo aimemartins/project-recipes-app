271672656a0938d0babedb255b474db8f4094df8
AIMÊ <aimemartins@hotmail.com>
2022-11-30 17:05:48 -0300

    adicionando rota favorite recipes

--- src/services/theCocktailDB.js
+++ src/services/theCocktailDB.js
@@ -6,14 +6,14 @@ export const theCocktailDBIngredient = async (ingredient) => {
 };
 
 export const theCocktailDBName = async (name) => {
-  const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${name}`;
+  const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`;
   const fetchTheMeal = await fetch(endpoint);
   const { drinks } = await fetchTheMeal.json();
   return drinks;
 };
 
 export const theCocktailDBFirstLet = async (firstLet) => {
-  const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${firstLet}`;
+  const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${firstLet}`;
   const fetchTheMeal = await fetch(endpoint);
   const { drinks } = await fetchTheMeal.json();
   return drinks;
--- src/services/theMealDB.js
+++ src/services/theMealDB.js
@@ -6,14 +6,14 @@ export const theMealDBIngredient = async (ingredient) => {
 };
 
 export const theMealDBName = async (name) => {
-  const endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${name}`;
+  const endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
   const fetchTheMeal = await fetch(endpoint);
   const { meals } = await fetchTheMeal.json();
   return meals;
 };
 
 export const theMealDBFirstLet = async (firstLet) => {
-  const endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${firstLet}`;
+  const endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?f=${firstLet}`;
   const fetchTheMeal = await fetch(endpoint);
   const { meals } = await fetchTheMeal.json();
   return meals;
